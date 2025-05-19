2f7ab039d46c49dd3421e507f56da1cc56970ba3
eightypoint1 <EzekielAaronMarmora@proton.me>
2025-05-19 11:52:18 +0700

    Membuat kelas person

---
+++ src/Person/Person.java
@@ -0,0 +1,19 @@
+public abstract class Person {
+    private String nama;
+    private String kelamin;
+    private String alamat;
+    private String role;
+    
+    public String getNama() {};
+    public String getKelamin() {};
+    public String getAlamat() {};
+    public String getRole() {};
+
+    public void setNama() {};
+    public void setKelamin() {};
+    public void setAlamat() {};
+    public void setRole() {};
+
+    @Override
+    public String toString() {};
+} 
