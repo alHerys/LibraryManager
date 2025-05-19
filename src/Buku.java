8ef12d53242b9fee17e59a51ce63012c8cd71f29
Hery <alfiantoherysarborn@gmail.com>
2025-05-19 11:53:54 +0700

    Menambahkan class buku

---
+++ src/Buku.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Buku<T> {
+    private String judul;
+    private List<String> penulis;
+    private T genre;
+    private String tahunTerbit;
+
+    public Buku() {
+        this.penulis = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                
+                """);
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public List<String> getPenulis() {
+        return penulis;
+    }
+
+    public void setPenulis(List<String> penulis) {
+        this.penulis = penulis;
+    }
+
+    public T getGenre() {
+        return genre;
+    }
+
+    public void setGenre(T genre) {
+        this.genre = genre;
+    }
+
+    public String getTahunTerbit() {
+        return tahunTerbit;
+    }
+
+    public void setTahunTerbit(String tahunTerbit) {
+        this.tahunTerbit = tahunTerbit;
+    }
+}
