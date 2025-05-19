00900adce13d3906fbe328a648dd717a1363f8ae
eightypoint1 <EzekielAaronMarmora@proton.me>
2025-05-19 12:47:43 +0700

    Menambahkan data field untuk kelas Manager

--- src/Manager.java
+++ src/Manager.java
@@ -1,2 +1,11 @@
+import java.util.*;
+
 public class Manager {
+    private List<Buku<?>> buku;
+    
+    private Set<String> genre;
+
+    private Queue<Buku<?>> pinjamanBuku;
+
+    private String tahunTerbit;
 }
